18aa583bd4dda86e7ba17407997bfe8f6be75bc7
Clara <claralobo13@gmail.com>
2019-01-31 16:02:32 +0530

    Updated File

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,25 @@ package com.example.clara.simpleproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+      Button b;
+      TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt=findViewById(R.id.txt);
+        b=findViewById(R.id.btn);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("Updated Text");
+                b.setVisibility(View.GONE);
+            }
+        });
     }
 }
